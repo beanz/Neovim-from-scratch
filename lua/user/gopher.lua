29604d1faad1954a797d2a1149f68e134cb66bdd
Mark Hindess <HINDESSM@uk.ibm.com>
2024-04-25 10:26:55 +0100

    chore: gopher

---
+++ lua/user/gopher.lua
@@ -0,0 +1,12 @@
+local M = {
+  "olexsmir/gopher.nvim",
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  }
+}
+
+function M.config() end
+
+return M

d43cd3f0cfd304c54efa807b4a5df07452c795de
Mark Hindess <HINDESSM@uk.ibm.com>
2024-04-25 19:37:40 +0100

    chore: neotest commands

--- lua/user/neotest.lua
+++ lua/user/neotest.lua
@@ -2,12 +2,14 @@ local M = {
   "nvim-neotest/neotest",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
+    "nvim-neotest/nvim-nio",
     -- general tests
     "vim-test/vim-test",
     "nvim-neotest/neotest-vim-test",
     -- language specific tests
     "marilari88/neotest-vitest",
     "nvim-neotest/neotest-python",
+    "nvim-neotest/neotest-go",
     "nvim-neotest/neotest-plenary",
     "rouge8/neotest-rust",
     "lawrence-laz/neotest-zig",
@@ -21,8 +23,12 @@ function M.config()
     ["<leader>tt"] = { "<cmd>lua require'neotest'.run.run()<cr>", "Test Nearest" },
     ["<leader>tf"] = { "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", "Test File" },
     ["<leader>td"] = { "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", "Debug Test" },
-    ["<leader>ts"] = { "<cmd>lua require('neotest').run.stop()<cr>", "Test Stop" },
+    ["<leader>ts"] = { "<cmd>lua require('neotest').run.run(vim.fn.getcwd())<cr>", "Test Suite" },
+    ["<leader>tS"] = { "<cmd>lua require('neotest').run.stop()<cr>", "Test Stop" },
     ["<leader>ta"] = { "<cmd>lua require('neotest').run.attach()<cr>", "Attach Test" },
+    ["<leader>ti"] = { "<cmd>lua require'neotest'.summary.toggle()<cr>", "Test Summary" },
+    ["<leader>to"] = { "<cmd>lua require('neotest').output.open()<cr>", "Output" },
+    ["<leader>tO"] = { "<cmd>lua require('neotest').output.open( { enter = true })<cr>", "Output(enter)" },
   }
 
   ---@diagnostic disable: missing-fields
@@ -33,11 +39,38 @@ function M.config()
       },
       require "neotest-vitest",
       require "neotest-zig",
+      require "neotest-go" {
+        recursive_run = true,
+      },
+      require "neotest-rust",
       require "neotest-vim-test" {
         ignore_file_types = { "python", "vim", "lua", "javascript", "typescript" },
       },
     },
+    icons = {
+      expanded = "",
+      child_prefix = "",
+      child_indent = "",
+      final_child_prefix = "",
+      non_collapsible = "",
+      collapsed = "",
+
+      passed = "",
+      running = "",
+      failed = "",
+      unknown = ""
+    },
   }
+
+  vim.cmd([[
+    command! NeotestSummary lua require("neotest").summary.toggle()
+    command! NeotestFile lua require("neotest").run.run(vim.fn.expand("%"))
+    command! Neotest lua require("neotest").run.run(vim.fn.getcwd())
+    command! NeotestNearest lua require("neotest").run.run()
+    command! NeotestDebug lua require("neotest").run.run({ strategy = "dap" })
+    command! NeotestAttach lua require("neotest").run.attach()
+    command! NeotestOutput lua require("neotest").output.open()
+  ]])
 end
 
 return M

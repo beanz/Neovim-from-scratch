7b5a7e9c2bf514fd414050708df76f04dae51e12
Mark Hindess <HINDESSM@uk.ibm.com>
2024-04-26 09:23:41 +0100

    chore: add gitlinker

--- lua/user/extras/gitlinker.lua
+++ lua/user/extras/gitlinker.lua
@@ -7,13 +7,25 @@ local M = {
 function M.config()
   local wk = require "which-key"
   wk.register {
-    ["<leader>gy"] = { "<cmd>GitLink!<cr>", "Git link" },
+    ["<leader>gy"] = { "<cmd>GitLink<cr>", "Git link" },
     ["<leader>gY"] = { "<cmd>GitLink blam<cr>", "Git link blame" },
   }
 
   require("gitlinker").setup {
     message = false,
     console_log = false,
+    action_callback = require"gitlinker.actions".copy_to_clipboard,
+    router = {
+      browse = {
+        ["^github%.ibm%.com"] = "https://github.ibm.com/"
+          .. "{_A.ORG}/"
+          .. "{_A.REPO}/blob/"
+          .. "{_A.REV}/"
+          .. "{_A.FILE}?plain=1" -- '?plain=1'
+          .. "#L{_A.LSTART}"
+          .. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
+      }
+    },
   }
 end
 
